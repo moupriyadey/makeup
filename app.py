11f4d0ad52b315ed9059a30a51130a3495efc1c3
Subhash S <covcorres@gmail.com>
2025-04-16 16:16:21 +0530

    Updated app with new port and changes

--- app.py
+++ app.py
@@ -216,3 +216,9 @@ def update_status(appointment_id, status):
 if __name__ == '__main__':
     app.run(debug=True)
 
+from waitress import serve
+from app import app  # Make sure 'app' is the Flask instance
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5001, debug=False, use_reloader=False)
+
